a8056547bac067a2151d3e5a9c631ce65d63ffc2
erol1098 <erol1098@gmail.com>
2022-07-12 19:13:29 +0300

    Login Page Updated

--- src/pages/Login/Login.styled.jsx
+++ src/pages/Login/Login.styled.jsx
@@ -1,14 +1,9 @@
 import styled from "styled-components";
-
 const StyledLogin = styled.section`
-  /* ... */
-
-  background-image: url("https://source.unsplash.com/random/1920x1280?sig=1");
-  background-position: center;
-  background-repeat: no-repeat;
+  background: url("https://images.unsplash.com/photo-1519671282429-b44660ead0a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")
+    no-repeat center center fixed;
   background-size: cover;
   height: 100%;
   padding: 1rem 0;
 `;
-
 export default StyledLogin;
